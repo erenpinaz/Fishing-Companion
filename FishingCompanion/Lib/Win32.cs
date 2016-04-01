5868a92dde2acbf0f1b92a37c94da79190c126e8
Eren Pinaz <pinaz.eren@gmail.com>
2016-04-01 21:03:53 +0300

    Initial commit

---
+++ FishingCompanion/Lib/Win32.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FishingCompanion.Lib
+{
+    public class Win32
+    {
+        // Message code definitions
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_LBUTTONUP = 0x0202;
+        private const int WM_RBUTTONDOWN = 0x0204;
+        private const int WM_RBUTTONUP = 0x0205;
+        private const int WM_KEYDOWN = 0x100;
+        private const int WM_KEYUP = 0x101;
+        private const int WK_FISHING = 0x31; // Key_1
+        private const int WK_BAIT = 0x32; // Key_2
+
+
+        [DllImport("User32.Dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.Dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("User32.Dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("User32.Dll")]
+        private static extern int SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("User32.Dll")]
+        private static extern long SetCursorPos(int x, int y);
+
+        [DllImport("User32.Dll")]
+        private static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        /// <summary>
+        /// Finds and returns window handle
+        /// </summary>
+        /// <param name="appName"></param>
+        /// <returns></returns>
+        public static IntPtr GetWindowHandle(string appName)
+        {
+            return FindWindow(null, appName);
+        }
+
+        /// <summary>
+        /// Brings given window to the front
+        /// </summary>
+        /// <param name="hWnd"></param>
+        public static void ActivateWindow(IntPtr hWnd)
+        {
+            SetForegroundWindow(hWnd);
+        }
+
+        /// <summary>
+        /// Returns current cursor information
+        /// </summary>
+        /// <returns></returns>
+        public static CURSORINFO GetCursor()
+        {
+            CURSORINFO curInfo;
+            curInfo.cbSize = Marshal.SizeOf(typeof (CURSORINFO));
+            GetCursorInfo(out curInfo);
+
+            return curInfo;
+        }
+
+        /// <summary>
+        /// Returns the position information of the cursor
+        /// </summary>
+        /// <returns></returns>
+        public static POINT GetCursorPosition()
+        {
+            CURSORINFO curInfo;
+            curInfo.cbSize = Marshal.SizeOf(typeof (CURSORINFO));
+            GetCursorInfo(out curInfo);
+
+            return curInfo.ptScreenPos;
+        }
+
+        /// <summary>
+        /// Returns current window rectangle
+        /// </summary>
+        /// <returns></returns>
+        public static RECT GetWindowRect(IntPtr hWnd)
+        {
+            RECT windowRect;
+            GetWindowRect(hWnd, out windowRect);
+
+            return windowRect;
+        }
+
+        /// <summary>
+        /// Resets the mouse coordinates and returns default game cursor
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        public static CURSORINFO GetDefaultGameCursor(IntPtr hWnd)
+        {
+            var windowRect = GetWindowRect(hWnd);
+
+            MoveMouse(windowRect.Right, windowRect.Bottom);
+            Thread.Sleep(100);
+
+            CURSORINFO curInfo;
+            curInfo.cbSize = Marshal.SizeOf(typeof (CURSORINFO));
+            GetCursorInfo(out curInfo);
+
+            return curInfo;
+        }
+
+        /// <summary>
+        /// Moves mouse to given position
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public static void MoveMouse(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        /// <summary>
+        /// Sends left mouse button click message
+        /// </summary>
+        /// <param name="hWnd"></param>
+        public static void SendMouseClick(IntPtr hWnd)
+        {
+            SendMessage(hWnd, WM_RBUTTONDOWN, 0, 0);
+            Thread.Sleep(200);
+            SendMessage(hWnd, WM_RBUTTONUP, 0, 0);
+        }
+
+        /// <summary>
+        /// Sends key press (Key_1) message
+        /// </summary>
+        /// <param name="hWnd"></param>
+        public static void CastFishing(IntPtr hWnd)
+        {
+            SendMessage(hWnd, WM_KEYDOWN, WK_FISHING, 0);
+            Thread.Sleep(200);
+            SendMessage(hWnd, WM_KEYUP, WK_FISHING, 0);
+        }
+
+        /// <summary>
+        /// Sends key press (Key_2) message
+        /// </summary>
+        /// <param name="hWnd"></param>
+        public static void CastBait(IntPtr hWnd)
+        {
+            SendMessage(hWnd, WM_KEYDOWN, WK_BAIT, 0);
+            Thread.Sleep(200);
+            SendMessage(hWnd, WM_KEYUP, WK_BAIT, 0);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CURSORINFO
+        {
+            public int cbSize;
+            public int flags;
+            public IntPtr hCursor;
+            public POINT ptScreenPos;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+    }
+}
---
+++ FishingCompanion/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CoreAudio;
+using FishingCompanion.Lib;
+
+namespace FishingCompanion
+{
+    internal class Program
+    {
+        private static Process _gameProcess;
+        private static IntPtr _hWnd;
+
+        private static int _baitDuration = 300;
+        private static int _fishingDuration = 21;
+
+        private static MMDeviceEnumerator _devEnum;
+        private static MMDevice _device;
+        private static AudioSessionControl2 _session;
+        private static readonly Queue<int> VolumeQueue = new Queue<int>(5);
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("[Initialization] -> Searching game process");
+
+            // Get the game process (32-bit)
+            _gameProcess = Process.GetProcessesByName("wow").FirstOrDefault();
+
+            if (_gameProcess == null)
+            {
+                Console.WriteLine("[Initialization] -> Unable to find 32-bit game process. Searching for 64-bit version");
+
+                // Get the game process (64-bit)
+                _gameProcess = Process.GetProcessesByName("wow-64").FirstOrDefault();
+            }
+
+            if (_gameProcess == null)
+            {
+                Console.WriteLine("[Initialization] -> Unable to find 64-bit game process. Searcing with window name");
+
+                // Get the game process by the window title
+                _gameProcess =
+                    Process.GetProcesses()
+                        .FirstOrDefault(p => p.MainWindowTitle.ToUpperInvariant() == "WORLD OF WARCRAFT");
+            }
+
+            if (_gameProcess == null)
+            {
+                Console.WriteLine("[Initialization] -> Unable to find game window. Press any key to exit.");
+                Console.ReadLine();
+
+                // Exit if process is not found
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("[Initialization] -> Game windows found. (PID : " + _gameProcess.Id + ", Title : " +
+                              _gameProcess.MainWindowTitle + ")");
+
+            // Process found get the handle
+            _hWnd = _gameProcess.MainWindowHandle;
+
+            // Bring game window to front
+            Console.WriteLine("[Initialization] -> Activating game window");
+            Win32.ActivateWindow(_hWnd);
+
+            // Setup game audio session (splash listener)
+            Console.WriteLine("[Initialization] -> Configuring audio session");
+            _devEnum = new MMDeviceEnumerator();
+            _device = _devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+
+            var sessionFound = false;
+            for (var i = 0; i < _device.AudioSessionManager2.Sessions.Count; i++)
+            {
+                _session = _device.AudioSessionManager2.Sessions[i];
+                if (_session.GetProcessID == _gameProcess.Id)
+                {
+                    sessionFound = true;
+                    break;
+                }
+            }
+
+            if (!sessionFound)
+            {
+                Console.WriteLine("[Initialization] -> Unable to find audio session. Press any key to exit.");
+                Console.ReadLine();
+
+                // Exit if audio session is not found
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("[Initialization] -> Audio session is ready");
+            }
+
+            // Start fishing
+            StartFishing();
+        }
+
+        /// <summary>
+        /// Starts fishing procedure
+        /// </summary>
+        private static async void StartFishing()
+        {
+            Task.Factory.StartNew(CastBaitTask);
+
+            while (true)
+            {
+                _fishingDuration = 21;
+
+                // Cast fishing
+                Console.WriteLine("[Procedure Manager] -> Casting fishing");
+                Win32.CastFishing(_hWnd);
+
+                // Scan fishing bobber
+                var bobberFound = await Task.FromResult(ScanBobberTask());
+                if (bobberFound)
+                {
+                    // Setup splash listener task
+                    Task.Factory.StartNew(ListenSplashTask).Wait();
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+            }
+        }
+
+        private static bool ScanBobberTask()
+        {
+            // Get current (default) cursor
+            var defaultCursor = Win32.GetDefaultGameCursor(_hWnd);
+
+            // Calculate scanning area
+            var wowRect = Win32.GetWindowRect(_hWnd);
+            var xMin = wowRect.Right/4;
+            var xMax = xMin*3;
+            var yMin = wowRect.Bottom/3;
+            var yMax = yMin*2;
+
+            // Scan for bobber
+            Console.WriteLine("[Procedure Manager] -> Scanning for bobber (Scan area x: [" + xMin + "-" + xMax +
+                              "], y: [" + yMin + "-" + yMax + "])");
+
+            var bobberFound = false;
+            while (xMin <= xMax && !bobberFound)
+            {
+                for (var i = yMin; i < yMax; i += 50)
+                {
+                    Win32.MoveMouse(xMin, i);
+
+                    Thread.Sleep(20);
+
+                    var currentCursor = Win32.GetCursor();
+                    if ((currentCursor.hCursor != defaultCursor.hCursor) ||
+                        currentCursor.flags != defaultCursor.flags)
+                    {
+                        Console.WriteLine("[Procedure Manager] -> Bobber found");
+                        bobberFound = true;
+                        break;
+                    }
+                }
+
+                xMin += 30;
+            }
+
+            return bobberFound;
+        }
+
+        /// <summary>
+        /// Casts bait every 5 minutes
+        /// </summary>
+        private static void CastBaitTask()
+        {
+            Console.WriteLine("[Task Manager] -> Bait caster task started");
+
+            Console.WriteLine("[Procedure Manager] -> Bait casted");
+            Win32.CastBait(_gameProcess.MainWindowHandle);
+
+            while (true)
+            {
+                Console.WriteLine("[Procedure Manager] -> Time left until bait ends: " + _baitDuration);
+
+                _baitDuration--;
+
+                if (_baitDuration == 0)
+                {
+                    Console.WriteLine("[Procedure Manager] -> Bait casted");
+                    Win32.CastBait(_gameProcess.MainWindowHandle);
+
+                    _baitDuration = 300;
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static void ListenSplashTask()
+        {
+            Console.WriteLine("[Thread Manager] -> Splash listener thread started");
+
+            while (true)
+            {
+                var currentVol = Convert.ToInt32(_session.AudioMeterInformation.MasterPeakValue*100);
+                VolumeQueue.Enqueue(currentVol);
+
+                if (VolumeQueue.Count > 5)
+                {
+                    VolumeQueue.Dequeue();
+                }
+
+                if (currentVol - Convert.ToInt32(VolumeQueue.Sum()/VolumeQueue.Count()) >= 13)
+                {
+                    Console.WriteLine("[Audio Manager] -> Splash sound heard");
+
+                    Console.WriteLine("[Procedure Manager] -> Looting fish");
+                    Win32.SendMouseClick(_gameProcess.MainWindowHandle);
+
+                    break;
+                }
+                _fishingDuration--;
+                Console.WriteLine("[Procedure Manager] -> Time left until fishing ends: " + _fishingDuration);
+
+                if (_fishingDuration == 0)
+                {
+                    Console.WriteLine("[Procedure Manager] -> Fishing ended");
+                    break;
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private enum ActionStatus
+        {
+            Idle,
+            Casting
+        }
+    }
+}
